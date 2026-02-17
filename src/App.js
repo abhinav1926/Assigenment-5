df2c574bd9f8c5b2130f3e864bbd7470e859aa49
abhinav1926 <patelabhinav1919@gmail.com>
2026-02-17 23:53:46 +0530

    Initial commit - Assignment 5 React Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PhotoGrid from './components/PhotoGrid';
+
+function App() {
+  return (
+    <div className="App">
+      <PhotoGrid />
+    </div>
+  );
+}
+
+export default App;
+
